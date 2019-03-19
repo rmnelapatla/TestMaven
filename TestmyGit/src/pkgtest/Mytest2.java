81a906a2dec6d60147e2bf268b400409898bebf2
rmnelapatla <rama_70@yahoo.com>
2019-03-18 22:46:14 -0500

    new file add

--- TestmyGit/src/pkgtest/Mytest2.java
+++ TestmyGit/src/pkgtest/Mytest2.java
@@ -2,6 +2,10 @@ package pkgtest;
 
 public class Mytest2 {
 
+
+	public void  test(){
+		System.out.println("test");
+	}
 	
 	public static void main(String ara[]){
 		System.out.println("this is first line");
---
+++ TestmyGit/src/pkgtest/Testgit4.java
@@ -0,0 +1,7 @@
+package pkgtest;
+
+/**
+ * Created by sumal on 3/18/2019.
+ */
+public class Testgit4 {
+}
